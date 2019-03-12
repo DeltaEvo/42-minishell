4d7b3902fb447174f5d61be1943fc3e876fff58b
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-03-12 15:05:07 +0100

    Error on setenv with = in var name

--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -6,19 +6,26 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 16:45:03 by dde-jesu          #+#    #+#             */
-/*   Updated: 2019/01/31 15:34:11 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2019/03/12 15:01:52 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
+#include "ft/str.h"
 
 #define USAGE "Usage: setenv [NAME] [VALUE]\n"
 #define ENV2BIG "Cannot add new env variable, env will be bigger than ARG_MAX\n"
+#define INVALIDNAME "Invalid env variable name\n"
 
 int	builtin_setenv(int argc, char **argv, struct s_shell *shell)
 {
 	if (argc == 3)
 	{
+		if (ft_strchr(argv[1], '='))
+		{
+			write(2, INVALIDNAME, sizeof(INVALIDNAME) - 1);
+			return (1);
+		}
 		if (sh_setenv(shell, argv[1], argv[2]))
 			return (0);
 		else
